c6a847641e5d5e6c420d682e2349213fcaf2b879
vipin kumar <kumarvipin24031999@gmail.com>
2021-11-22 15:41:26 +0530

    pattern-19

---
+++ 19 half pyramid mirror image using numbers/half pyramid mirror image using numbers.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>				// including header files
+int main(void)				// main function
+{
+	int row,col,space,length;					// declaration of integer type variables i,j and length
+	printf("enter the length of the pattern");			// printing message on output screen for length
+	scanf("%d",&length);							// recording the user's input length in length variable
+	printf("\n");					// new line escape sequence
+
+	return 0;
+}
